24882429fa7d3cbf540c6f86e41c53400a261ec1
Chiranjeevi-TFP576 <p.chiranjeevi12345@gmail.com>
2023-01-09 14:19:55 +0530

    [chiru] Using CompareTo and Equals Method

--- Line Comparisons/Line Comparisons/Program.cs
+++ Line Comparisons/Line Comparisons/Program.cs
@@ -1,7 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Line_Comparisons;
+Console.WriteLine(" 1-LengthOfTheLine \n 2-TwoLinesAreEqual\n 3-CompareTwoLineswithOperaors\n 4-CompareToMethod");
+int x = Convert.ToInt32(Console.ReadLine());
+switch(x)
+{
+    case 1:
+        //Console.WriteLine("Welcome To Line Comparisons Problems");
+        //UC1_LengthOfTheLine.LengthOfLines();
+        break;
+    case 2:
+        //UC2_TwoLinesEqual.TwoLinesAreEquals();
 
-//Console.WriteLine("Welcome To Line Comparisons Problems");
-//UC1_LengthOfTheLine.LengthOfLines();
-//UC2_TwoLinesEqual.TwoLinesAreEquals();
-UC3_Compare_Two_Lines.ComparedTwoLines();
+        break;
+    case 3:
+        //UC3_Compare_Two_Lines.ComparedTwoLines();
+        break;
+    case 4:
+        UC4_CompareToTwoLines.CompareToMethod();
+        break;
+    default:
+        Console.WriteLine("Complete the programme");
+        break;
+}
+
+
+
+
